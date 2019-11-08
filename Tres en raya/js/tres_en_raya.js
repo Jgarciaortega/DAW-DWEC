cfd9fd63a74d24c5b70d2b7f8368c2a79504ed9c
Javier Garcia Ortega <txhavi@gmail.com>
2019-11-08 18:56:12 +0100

    falta comprobacion fin partida

--- Tres en raya/js/tres_en_raya.js
+++ Tres en raya/js/tres_en_raya.js
@@ -7,7 +7,6 @@ var turnoCPU = false;
 
 
 
-
 function crearTablero() {
 
     const nodoPadre = document.querySelector("#tablero");
@@ -28,10 +27,7 @@ function crearTablero() {
             nodoPadre.appendChild(casilla);
 
         }
-
     }
-
-
 }
 
 function marcarCasilla(e) {
@@ -45,7 +41,6 @@ function marcarCasilla(e) {
         div.classList.add("cruz");
         turnoCPU = true;
     }
-
 }
 
 
@@ -54,35 +49,72 @@ function eligeCPU() {
     let tablero = document.querySelectorAll(".casilla");
     console.log(tablero);
     let cont = 0;
+    let divValido = false;
 
-    for (let y = 0; y < 3; y++) {
+    while (!divValido) {
 
-        for (let x = 0; x < 3; x++) {
+        let y = Math.floor(Math.random() * (3));
+        let x = Math.floor(Math.random() * (3));
 
-            let div = document.getElementById(y + " " + x);
+        console.log(x + " " + y);
+        let div = document.getElementById(y + " " + x);
 
-            if (div.classList.contains("circulo")){
+        if (!div.classList.contains("circulo") && !div.classList.contains("cruz")) {
 
-                if(x != 2){
-
-                    let div2 = document.getElementById(y + " " + x+1);                
-                    div2.classList.add("circulo");
-                } 
-
-            }else{
-
-                
-            }
+            div.classList.add("circulo");
+            divValido = true;
 
         }
+
     }
-
-
     turnoCPU = false;
 }
 
 
 
+function comprobarVictoria() {
+
+    let contCirculo = 0;
+    let contCruz = 0;
+
+    //COMPROBACION HORIZONTAL
+
+    for (let x = 0; x < 3; x++) {
+
+        for (let y = 0; y < 3; y++) {
+
+            let casilla = document.getElementById(y + " " + x);
+
+            
+
+        }
+
+    }
+
+    //COMPROBACION VERTICAL
 
 
+    for (let x = 0; x < 3; x++) {
 
+        for (let y = 0; y < 3; y++) {
+
+            
+
+        }
+
+    }
+
+    //COMPROBACION DIAGONAL
+
+    for (let x = 0; x < 3; x++) {
+
+        for (let y = 0; y < 3; y++) {
+
+            
+
+        }
+
+    }
+
+
+}
