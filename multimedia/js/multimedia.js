d27cd29b7257314278ab0a612a79b06abbf40cb2
Javier Garcia Ortega <txhavi@gmail.com>
2019-11-21 20:40:34 +0100

    ok

--- multimedia/js/multimedia.js
+++ multimedia/js/multimedia.js
@@ -2,24 +2,49 @@
 
 function init() {
 
+    videoRunning = false;
+
+    video = document.querySelector('video');
     document.getElementById('play').addEventListener('click', play);
     document.getElementById('reiniciar').addEventListener('click', reiniciar);
     document.getElementById('mute').addEventListener('click', mutear);
     document.getElementById('volUp').addEventListener('click', modificarVolumen);
     document.getElementById('volDown').addEventListener('click', modificarVolumen);
+    document.getElementById('rew').addEventListener('click', modificarTiempo);
+    document.getElementById('for').addEventListener('click', modificarTiempo);
+    
+}
 
+
+function modificarTiempo(){
+
+    if(this.id == 'rew') video.currentTime-=10;
+    if(this.id == 'for') video.currentTime+=10;
 
 }
 
+
 function play() {
 
-    document.querySelector('video').play();
+    if(!videoRunning){
 
+        videoRunning = true;
+        video.play();
+        this.setAttribute('id','pause');
+       
+
+    }else{
+
+        videoRunning = false;
+        video.pause();
+        this.setAttribute('id','play');
+        
+    } 
+   
 }
 
 function mutear() {
 
-    let video = document.querySelector('video');
 
     if(video.muted){
 
@@ -29,19 +54,17 @@ function mutear() {
 
         video.muted = true;
     }
-   
+
 }
 
 function reiniciar(){
 
-    document.querySelector('video').load();
+    video.load();
     play();
     
 }
 
 function modificarVolumen(){
-
-    let video = document.querySelector('video');
 
     //TODO Tratar la excepcion de index size volume
      if(this.id == 'volUp' && video.volume <1) video.volume += 0.1;
@@ -49,5 +72,9 @@ function modificarVolumen(){
      if(this.id == 'volDown' && video.volume >0) video.volume -= 0.1; console.log(video.volume);
 
 }
+
+//VARIABLES GLOBALES
+let video;
+let videoRunning;
 
 window.onload = init;
